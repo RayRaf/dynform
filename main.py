99efde8135a508537f4b569d7c608797a0da299e
Rail Rafikov <rayraf@example.com>
2023-03-22 20:56:11 +0300

    Added new xls files and something new

--- main.py
+++ main.py
@@ -2,69 +2,43 @@ import xlrd
 from tkinter import *
 from tkinter import ttk
 
-labels_values = []
+root = Tk()
+root.title("RAY")
+
+type_names = []
 comboboxes = []
 selections = []
 
-book = xlrd.open_workbook("DB.xls")
+book = xlrd.open_workbook("DB1.xls")
 print("The number of worksheets is {0}".format(book.nsheets))
 print("Worksheet name(s): {0}".format(book.sheet_names()))
 sh = book.sheet_by_index(0)
 print("Лист {0} имеет  {1} строк и {2} столбцов".format(sh.name, sh.nrows, sh.ncols))
 print("Cell A1 is {0}".format(sh.cell_value(rowx=0, colx=0)))
-for rx in range(sh.ncols):
-    print(sh.cell_value(rowx=0, colx=rx))
-    labels_values.append(sh.cell_value(rowx=0, colx=rx))
-
-root = Tk()
-root.title("RAY")
-# root.geometry("250x200")
-# btn = ttk.Button(text="Button")
-# btn = ttk.Button(text="Button", command=click)
-
-# i = 0
-# for val in labelsvalues:
-#     label = ttk.Label(text=val)
-#     label.grid(row=i, column=0)
-#     combobox = ttk.Combobox(values=['Да', 'Нет'])
-#     combobox.grid(row=i, column=1)
-#     print(str(i))
-#     i += 1
+for rx in range(sh.nrows):
+    # print(sh.cell_value(rowx=0, colx=rx))
+    type_names.append(sh.cell_value(rowx=rx, colx=0))
 
 
 def selected(event):
-    i1 = 0
-    for combobox in comboboxes:
-        if combobox.get() != selections[i1]:
-            print('Изменен комбобокс ' + str(i1))
-            selections[i1] = combobox.get()
-        i1 += 1
+    selected_type = type_names.index(typ_select_combobox.get())
+
+    book2 = xlrd.open_workbook("DB{0}.xls". format(str(selected_type+1)))
+    sh1 = book2.sheet_by_index(0)
+    for rx1 in range(sh1.nrows):
+        comboboxes.append(ttk.Combobox(values=['Да', 'Нет']))
+        comboboxes[rx1].grid(row=rx1, column=1)
+        label = ttk.Label(text=sh1.cell_value(rowx=rx1, colx=0))
+        label.grid(row=rx1, column=0)
 
 
 
-    # for selection in selections:
-    #     if selection != selections[i1]:
-    #         label["text"] = f"вы выбрали: {selections[i1]} в комбобоксе"
-    #         # label.grid(row=5, column=1)
-    #         selections[i1] = selection
+label = ttk.Label(text="Тип схемы:")
+label.grid(row=0, column=0)
 
-
-    # label["text"] = f"вы выбрали: {selections} в комбобоксе"
-    # label.grid(row=1, column=1)
-
-
-i = 0
-
-
-for val in labels_values:
-    label = ttk.Label(text=val)
-    label.grid(row=i, column=0)
-    comboboxes.append(ttk.Combobox(values=['Да', 'Нет']))
-    comboboxes[i].grid(row=i, column=1)
-    comboboxes[i].bind("<<ComboboxSelected>>", selected)
-    selections.append(comboboxes[i].get())
-    print(str(i))
-    i += 1
+typ_select_combobox = ttk.Combobox(values=type_names)
+typ_select_combobox.grid(row=0, column=1)
+typ_select_combobox.bind("<<ComboboxSelected>>", selected)
 
 
 
@@ -74,20 +48,49 @@ for val in labels_values:
 
 
 
-# def single_click(event):
-#     combobox = ttk.Combobox(values=languages)
-#     combobox.pack(anchor=NW, padx=6, pady=6)
+
+
+#
+# def selected(event):
+#     i1 = 0
+#     for combobox in comboboxes:
+#         if combobox.get() != selections[i1]:
+#             print('Изменен комбобокс ' + str(i1))
+#             selections[i1] = combobox.get()
+#         i1 += 1
 #
 #
-# def double_click(event):
-#     btn["text"] = "Double Click"
-
-
-# btn = ttk.Button(text="Click")
-# btn.pack(anchor=CENTER, expand=1)
+# i = 0
+# for val in type_names:
+#     label = ttk.Label(text=val)
+#     label.grid(row=i, column=0)
+#     comboboxes.append(ttk.Combobox(values=['Да', 'Нет']))
+#     comboboxes[i].grid(row=i, column=1)
+#     comboboxes[i].bind("<<ComboboxSelected>>", selected)
+#     selections.append(comboboxes[i].get())
+#     print(str(i))
+#     i += 1
 #
-# btn.bind("<ButtonPress-1>", single_click)
-# btn.bind("<Double-ButtonPress-1>", double_click)
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 root.mainloop()
-
