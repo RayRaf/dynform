8a27dd738ebe6db55770f7c7781605b78245d41d
Rail Rafikov <rayraf@example.com>
2023-03-23 21:56:46 +0300

    Попытка реализации поиска. Пока безуспешная

--- main.py
+++ main.py
@@ -2,81 +2,73 @@ import xlrd
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title("RAY")
 
-type_names = []
-comboboxes = []
-selections = []
+class Glavn:
+    def __init__(self, foo=None):
+        self._foo = foo
 
-book = xlrd.open_workbook("DB1.xls")
-print("The number of worksheets is {0}".format(book.nsheets))
-print("Worksheet name(s): {0}".format(book.sheet_names()))
-sh = book.sheet_by_index(0)
-print("Лист {0} имеет  {1} строк и {2} столбцов".format(sh.name, sh.nrows, sh.ncols))
-print("Cell A1 is {0}".format(sh.cell_value(rowx=0, colx=0)))
-for rx in range(sh.nrows):
-    # print(sh.cell_value(rowx=0, colx=rx))
-    type_names.append(sh.cell_value(rowx=rx, colx=0))
-
-
-def selected(event):
-    selected_type = type_names.index(typ_select_combobox.get())
-
-    book2 = xlrd.open_workbook("DB{0}.xls". format(str(selected_type+1)))
-    sh1 = book2.sheet_by_index(0)
-    for rx1 in range(sh1.nrows):
-        comboboxes.append(ttk.Combobox(values=['Да', 'Нет']))
-        comboboxes[rx1].grid(row=rx1, column=1)
-        label = ttk.Label(text=sh1.cell_value(rowx=rx1, colx=0))
-        label.grid(row=rx1, column=0)
+    def Run (self):
 
 
 
-label = ttk.Label(text="Тип схемы:")
-label.grid(row=0, column=0)
+        root = Tk()
+        root.title("RAY")
 
-typ_select_combobox = ttk.Combobox(values=type_names)
-typ_select_combobox.grid(row=0, column=1)
-typ_select_combobox.bind("<<ComboboxSelected>>", selected)
+        type_names = []
+        comboboxes = []
+        selected_params = []
+        selected_type = None
 
+        book = xlrd.open_workbook("DB1.xls")
+        print("The number of worksheets is {0}".format(book.nsheets))
+        print("Worksheet name(s): {0}".format(book.sheet_names()))
+        sh = book.sheet_by_index(0)
+        print("Лист {0} имеет  {1} строк и {2} столбцов".format(sh.name, sh.nrows, sh.ncols))
+        print("Cell A1 is {0}".format(sh.cell_value(rowx=0, colx=0)))
+        for rx in range(sh.nrows):
+            type_names.append(sh.cell_value(rowx=rx, colx=0))
 
+        # ШАГ 1. Выбор тиа схемы
 
+        def type_selected(event):
+            selected_type = type_names.index(typ_select_combobox.get())
+            book2 = xlrd.open_workbook("DB{0}.xls".format(str(selected_type + 2)))
+            self._foo = book2.sheet_by_index(0)
+            for rx1 in range(self._foo.nrows):
+                if rx1 > 0:
+                    comboboxes.append(ttk.Combobox(values=['Да', 'Нет']))
+                    comboboxes[rx1 - 1].grid(row=rx1 - 1, column=1)
+                    comboboxes[rx1 - 1].bind("<<ComboboxSelected>>", param_selected)
+                    label1 = ttk.Label(text=self._foo.cell_value(rowx=rx1, colx=0))
+                    label1.grid(row=rx1 - 1, column=0)
 
+        label = ttk.Label(text="Тип схемы:")
+        label.grid(row=0, column=0)
+        type_names.pop(0)
+        typ_select_combobox = ttk.Combobox(values=type_names)
+        typ_select_combobox.grid(row=0, column=1)
+        typ_select_combobox.bind("<<ComboboxSelected>>", type_selected)
 
-
-
-
-
-
-
-#
-# def selected(event):
-#     i1 = 0
-#     for combobox in comboboxes:
-#         if combobox.get() != selections[i1]:
-#             print('Изменен комбобокс ' + str(i1))
-#             selections[i1] = combobox.get()
-#         i1 += 1
-#
-#
-# i = 0
-# for val in type_names:
-#     label = ttk.Label(text=val)
-#     label.grid(row=i, column=0)
-#     comboboxes.append(ttk.Combobox(values=['Да', 'Нет']))
-#     comboboxes[i].grid(row=i, column=1)
-#     comboboxes[i].bind("<<ComboboxSelected>>", selected)
-#     selections.append(comboboxes[i].get())
-#     print(str(i))
-#     i += 1
-#
-#
-#
-#
-#
-#
-#
+        # ШАГ 2. Проверка параметров
+        def param_selected(event, ):
+            selected_params.clear()
+            for combobox in comboboxes:
+                selected_params.append(combobox.get())
+            if selected_params.count('') == 0:
+                status_label = ttk.Label(text="Все поля заполнены")
+                status_label.grid(row=len(selected_params) + 1, column=0)
+                for col_index in range(self._foo.ncols):
+                    if col_index > 0:
+                        for row_index in range(self._foo.nrows):
+                            if row_index > 0:
+                                no = (self._foo.cell_value(rowx=row_index, colx=col_index) == '')
+                                yes = (self._foo.cell_value(rowx=row_index, colx=col_index) != '')
+                                if selected_params[row_index]:
+                                    if row_index == len(selected_params):
+                                        print('Match found')
+                                else:
+                                    print('Прервано')
+                                    break
 
 
 
@@ -85,6 +77,15 @@ typ_select_combobox.bind("<<ComboboxSelected>>", selected)
 
 
 
+            else:
+                status_label = ttk.Label(text="Не все поля заполнены")
+                status_label.grid(row=len(selected_params) + 1, column=0)
+
+        root.mainloop()
+
+
+a = Glavn()
+a.Run()
 
 
 
@@ -93,4 +94,10 @@ typ_select_combobox.bind("<<ComboboxSelected>>", selected)
 
 
 
-root.mainloop()
+
+
+
+
+
+
+
